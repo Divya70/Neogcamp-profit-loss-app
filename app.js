ed536d5448776c4cc77f3d4c17482e77159f4783
Divya Namdev <divyanamdev8795@gmail.com>
2021-08-26 00:41:08 +0530

    initial commit
    building logic

---
+++ app.js
@@ -0,0 +1,11 @@
+const initialPrice = document.querySelector('#initial-price');
+const stocksQuantity = document.querySelector('#stocks-quantity');
+const currentPrice = document.querySelector('#current-price');
+const submitBtn = document.querySelector('#submit-btn');
+const outputEl = document.querySelector('#output');
+
+function calculateProfitAndLoss(initial , quantity, current){
+
+}
+
+submitBtn.addEventListener("click", calculateProfitAndLoss);
